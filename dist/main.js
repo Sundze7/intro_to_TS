4d2bf56eddc4f0eb0f88beb552aa4c6acf22e80f
Sundze <keyonmwesundze@mail.com>
2023-08-21 04:36:35 +0100

    variables

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,10 @@
-var a = "1";
-console.log("aaa", a);
+// const a = "1";
+// console.log("aaa", a)
+//1. Variables
+// like in js, we define variables using const, var & let
+var hello = "world";
+// using const, u are not allowed to reassign the value
+var tim = "now";
+tim = "after";
+//NB: always specify type with variable. this makes ur code easy to read
+// 2.
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,15 @@
-const a = "1";
-console.log("aaa", a)
+// const a = "1";
+// console.log("aaa", a)
+
+//1. Variables
+
+// like in js, we define variables using const, var & let
+const hello: string = "world"
+// using const, u are not allowed to reassign the value
+let tim: string = "now"
+tim = "after"
+
+//NB: always specify type with variable. this makes ur code easy to read
+
+
+// 2.
