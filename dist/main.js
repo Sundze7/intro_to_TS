3cc54958392cec2dd0039ab7dad6d7bf73137229
Sundze <keyonmwesundze@mail.com>
2023-08-22 05:06:31 +0100

    fuctions and interfaces

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,17 @@
 // const a = "1";
 // console.log("aaa", a)
-//1. Variables
+//1. VARIABLES
 // like in js, we define variables using const, var & let
 var hello = "world";
 // using const, u are not allowed to reassign the value
 var tim = "now";
 tim = "after";
 //NB: always specify type with variable. this makes ur code easy to read
-// 2.
+// 2. FUNCTIONS
+var getFullName = function (name, surname) {
+    return name + " " + surname;
+};
+console.log(getFullName("Sundze", "Mustapha"));
+var user1 = {
+    name: "Tris"
+};
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 // const a = "1";
 // console.log("aaa", a)
 
-//1. Variables
+//1. VARIABLES
 
 // like in js, we define variables using const, var & let
 const hello: string = "world"
@@ -12,4 +12,21 @@ tim = "after"
 //NB: always specify type with variable. this makes ur code easy to read
 
 
-// 2.
+// 2. FUNCTIONS
+const getFullName = (name: string, surname: string): string => {
+    return name + " " + surname
+}
+console.log(getFullName("Sundze", "Mustapha"))
+
+// 3. INTERFACE
+// interface in TS is like a model or blueprint for creating entities.
+// interface name is Capitalized and by default, their properties are mandatory. Use "?" at the end of property name to make it optional e.g: "age?: number"
+
+interface User{
+    name: string
+    age?: number
+}
+const user1: User = {
+    name: "Tris"
+    
+}
